3d3ad153dd22ec8393f821223551ec5f921ceea4
shin.katsumaru <shin.katsumaru@supership.jp>
2018-06-27 11:24:30 +0900

    remove NS state manage

--- gcp_dns.tf
+++ gcp_dns.tf
@@ -1,11 +1,6 @@
-resource "google_dns_managed_zone" "katsukamaru_work" {
-  name        = "work"
-  dns_name    = "katsukamaru.work."
-}
-
 resource "google_dns_record_set" "development" {
-  managed_zone = "${google_dns_managed_zone.katsukamaru_work.name}"
-  name         = "${google_dns_managed_zone.katsukamaru_work.dns_name}"
+  managed_zone = "work"
+  name         = "katsukamaru.work."
   type         = "A"
   ttl          = 300
   rrdatas      = [
